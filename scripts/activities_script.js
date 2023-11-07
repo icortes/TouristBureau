675eee13ec4d2a39a65e85d9825ec70527bfcab9
Isaac Cortes Hernandez <isaaccortes014@gmail.com>
2023-11-07 12:24:34 -0800

    payment form

--- scripts/activities_script.js
+++ scripts/activities_script.js
@@ -6,24 +6,30 @@ function fillActivityCategories(activityCategory) {
   });
 }
 
-function showActivitiesAndFill(activityCategory, activityNameGroup, activityName) {
-  let selectedActivity = activityCategory.value;
+function showActivitiesAndFill(
+  activityCategory,
+  activityNameGroup,
+  activityName,
+  activityFooter
+) {
+  let selectedCategory = activityCategory.value;
   activityCategory.addEventListener('change', () => {
-    selectedActivity = activityCategory.value;
+    selectedCategory = activityCategory.value;
     console.log(`Activity Category Selected: ${activityCategory.value}`);
-    if (selectedActivity != '') {
+    activityFooter.classList.add('d-none');
+    if (selectedCategory != '') {
       activityNameGroup.classList.remove('d-none');
-      fillBasedOnCategory(selectedActivity, activityName);
+      fillBasedOnCategory(selectedCategory, activityName);
     } else {
       activityNameGroup.classList.add('d-none');
     }
   });
 }
 
-function fillBasedOnCategory(selectedActivity, activityName) {
+function fillBasedOnCategory(selectedCategory, activityName) {
   activityName.innerHTML = `<option value="" selected>Select One...</option>`;
   let filteredActivities = activities.filter((activity) => {
-    return activity.category == selectedActivity;
+    return activity.category == selectedCategory;
   });
   filteredActivities.forEach((activity) => {
     activityName.appendChild(new Option(activity.name, activity.id));
@@ -31,16 +37,51 @@ function fillBasedOnCategory(selectedActivity, activityName) {
 }
 
 function showSelectedActivityAndPaymentForm(activityName, activityFooter) {
-  let activity = activityName.value;
+  let activityId = activityName.value;
+  let selectedActivity = {};
   activityName.addEventListener('change', () => {
-    activity = activityName.value;
-    console.log(`Activity Selected: ${activity}`);
-    if (activity != '') {
+    activityId = activityName.value;
+    console.log(`Activity Selected: ${activityId}`);
+    if (activityId != '') {
       activityFooter.classList.remove('d-none');
+      selectedActivity = fillCard(activityId);
+      //display payment if price is > 0
+      console.log(`Selected Activity Name: ${selectedActivity.name}`);
+      if (selectedActivity.price > 0) {
+        console.log(`Price: $${selectedActivity.price}`);
+        displayPaymentForm();
+      }
     } else {
       activityFooter.classList.add('d-none');
     }
   });
+}
+/**
+ * Fills card content with selected activity information
+ * @param {string} activity
+ * @returns selected activity object
+ */
+function fillCard(activity) {
+  let selectedActivityArray = activities.filter((item) => {
+    return activity == item.id;
+  });
+
+  let selectedActivityObj = selectedActivityArray[0];
+
+  //get card elements
+  document.getElementById('card-name').textContent = selectedActivityObj.name;
+  document.getElementById('card-id').textContent = selectedActivityObj.id;
+  document.getElementById('card-title').textContent = selectedActivityObj.category;
+  document.getElementById('card-location').textContent = selectedActivityObj.location;
+  document.getElementById('card-price').textContent = `$${selectedActivityObj.price}`;
+  document.getElementById('card-description').textContent =
+    selectedActivityObj.description;
+
+  return selectedActivityObj;
+}
+
+function displayPaymentForm() {
+  
 }
 
 onload = () => {
@@ -50,6 +91,11 @@ onload = () => {
   let activityFooter = document.getElementById('activity-footer');
 
   fillActivityCategories(activityCategory);
-  showActivitiesAndFill(activityCategory, activityNameGroup, activityName);
+  showActivitiesAndFill(
+    activityCategory,
+    activityNameGroup,
+    activityName,
+    activityFooter
+  );
   showSelectedActivityAndPaymentForm(activityName, activityFooter);
 };
